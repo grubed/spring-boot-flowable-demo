0a4daefdbc1645abfecd0ef561844e0f52668ff5
clj198606061111@163.com <clj-1986>
2019-08-25 23:20:51 +0800

    init commit

---
+++ src/main/java/com/itclj/controller/FlowableController.java
@@ -0,0 +1,9 @@
+package com.itclj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/flowable")
+public class FlowableController {
+}
